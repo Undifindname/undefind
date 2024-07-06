4653c430e5c212f2ada9b4bd799b0ce98da4a87a
Undifindname <bilalzouine0@gmail.com>
2024-07-06 19:02:22 +0100

    Add files via upload

---
+++ newProge/script/script.js
@@ -0,0 +1,16 @@
+let container = document.querySelector('#container')
+for (let index = 1; index < 26; index++) {
+    let img = document.createElement('img');
+    img.src = `newProge/images/${index}.jpg`
+    img.classList = 'img'
+    container.appendChild(img)
+}
+window.addEventListener('load',function(){
+    let scrollY = sessionStorage.getItem('scrolY')
+    if(scrollY != null){
+        this.scrollTo(0,scrollY)
+    }
+})
+window.addEventListener('scroll',function(){
+     sessionStorage.setItem('scrolY',this.scrollY)
+})
