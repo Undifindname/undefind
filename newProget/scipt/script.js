f9e53cc96b0b7945cf9ce35ab5fe42b29b7e8299
Undifindname <bilalzouine0@gmail.com>
2024-07-06 07:40:10 +0100

    Add files via upload

---
+++ newProget/scipt/script.js
@@ -0,0 +1,16 @@
+let container = document.querySelector('#container')
+for (let index = 1; index < 26; index++) {
+    let img = document.createElement('img');
+    img.src = `images/${index}.jp`
+    img.classList = 'img'
+    container.appendChild(img)
+}
+window.addEventListener('load',function(){
+    let scrollY = sessionStorage.getItem('scrolY')
+    if(scrollY != null){
+        this.scrollTo(0,scrollY)
+    }
+})
+window.addEventListener('scroll',function(){
+     sessionStorage.setItem('scrolY',this.scrollY)
+})
